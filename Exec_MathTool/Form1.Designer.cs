4003856d203d834ecd58929e1991015dfe9c5ae4
ispan <ispan@DESKTOP-PAKSETB>
2022-10-26 16:55:51 +0800

    加入專案檔案。

---
+++ Exec_MathTool/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Exec_MathTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numbercboBox = new System.Windows.Forms.ComboBox();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // numbercboBox
+            // 
+            this.numbercboBox.FormattingEnabled = true;
+            this.numbercboBox.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"});
+            this.numbercboBox.Location = new System.Drawing.Point(21, 32);
+            this.numbercboBox.Name = "numbercboBox";
+            this.numbercboBox.Size = new System.Drawing.Size(68, 20);
+            this.numbercboBox.TabIndex = 0;
+            // 
+            // calcButton
+            // 
+            this.calcButton.Location = new System.Drawing.Point(140, 30);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(75, 23);
+            this.calcButton.TabIndex = 1;
+            this.calcButton.Text = "button1";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(21, 73);
+            this.resultTextBox.Multiline = true;
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.ReadOnly = true;
+            this.resultTextBox.Size = new System.Drawing.Size(194, 206);
+            this.resultTextBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(242, 291);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.numbercboBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox numbercboBox;
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.TextBox resultTextBox;
+    }
+}
+
---
+++ Exec_MathTool/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exec_MathTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "單一乘法表";
+            this.MaximizeBox = this.MinimizeBox = false;
+            this.FormBorderStyle = FormBorderStyle.Fixed3D;
+            this.StartPosition = FormStartPosition.CenterParent;
+
+            calcButton.Text = "計算";
+
+            resultTextBox.Font = new Font("標楷體", 12, FontStyle.Bold);
+            //numbercboBox.SelectedIndex = 0;
+
+        }
+
+        private void calcButton_Click(object sender, EventArgs e)
+        {
+            // 
+        }
+    }
+}
